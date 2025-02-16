61764dd3b934cbd8c56be900f70ff4b40537cb23
dghfhdgbxgbg <134328176+dghfhdgbxgbg@users.noreply.github.com>
2025-02-16 14:49:21 +0545

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID","27655384"))
 API_HASH = getenv("API_HASH","a6a418b023a146e99af9ae1afd571cf4")
-BOT_TOKEN = getenv("BOT_TOKEN","8106761204:AAEoorExM6k1YrGpgifIqzbukgERjfEDnrw")
+BOT_TOKEN = getenv("BOT_TOKEN","7779804166:AAG-93Bw1D0hbuuwLf1LErxQPmBAa59Dhwg")
 BANALL = list(map(int, getenv("BANALL", "7137269276 7045191057 7091230649").split()))
